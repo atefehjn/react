48864fc6d71c60ceb330de4181cc2d4a9175ba3a
atefe_hjn97 <atefe_hajian97@yahoo.com>
2024-09-15 16:58:07 +0330

    sunday work

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -1,4 +1,4 @@
-const data = [
+/*const data = [
 	{
 		id: 1,
 		title: 'The Lord of the Rings',
@@ -144,7 +144,7 @@ function getBook(id) {
 }
 
 // Distructuring
-const book = getBook(1);
+const book = getBook(3);
 const tilte1 = book.title;
 const author1 = book.author;
 
@@ -188,3 +188,112 @@ year;
 
 const getYearArrow = (str) => str.split('-')[0];
 console.log(getYearArrow(book.publicationDate));
+
+//ternary instead of is statement:
+if (book.pages > 1000) {
+	console.log('over than 1000');
+} else {
+	console.log('less than 1000');
+}
+//try this:
+pageRange = book.pages > 1000 ? 'over than 1000' : 'less than 1000';
+console.log(`the ${book.title} has ${pageRange} pages`);
+//short circuiting and logical oprators like &&, || and ??
+
+//falsy values: 0,'',undefined,null
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || 'no data';
+// countWrong;
+// it is wrong because it has count but is zero
+//for solving this problem make another operator named nullish coalescing with symbol ??
+// const count = book.reviews.librarything.reviewsCount ?? 'no data';
+// count;
+
+//optional chaining
+//when we are not sure a property is defined or not, we can use optional operand:
+//for example we write a function that calculate all review counts for each book:
+//for book 3 we dont have reviewCount for librarything and we get this error:
+// Cannot read properties of undefined (reading 'reviewsCount)
+//for solving we use optional chaining operand, this check if a propertiy does exist, then check the value of it, and if it not exist, it ignore it
+//the oprand is ? after each property
+function totalReviewCount(book) {
+	const goodreadCount = book.reviews?.goodreads?.reviewsCount;
+	const librarythingCount = book.reviews?.librarything?.reviewsCount ?? 0;
+	return goodreadCount + librarythingCount;
+}
+console.log(totalReviewCount(book));
+
+// Array map method
+books = getBooks();
+
+//map method little example
+const a = [1, 2, 3, 4, 5];
+const mapped_a = a.map((el) => el * 2);
+mapped_a;
+const titles = books.map((book) => book.title);
+titles;
+
+const essential_data = books.map((book) => ({
+	title: book.title,
+	author: book.author,
+}));
+essential_data;
+
+//filter method on Array:
+
+const longBookWithMovie = books
+	.filter((book) => book.pages > 500)
+	.filter((book) => book.hasMovieAdaptation)
+	.map((book) => ({
+		title: book.title,
+		author: book.author,
+		genre: book.genres,
+	}));
+longBookWithMovie;
+
+//reduse method
+//actully is sum of pages
+const AllBooksPages = books.reduce((acc, book) => acc + book.pages, 0);
+AllBooksPages;
+
+//sort method, it is a functional method and when use sort method, change main array's elements also:
+// a - b :Ascending
+// b - a :Descending
+const arr = [4, 1, 6, 3, 8, 4, 9];
+// const sorted_arr = arr.sort((a, b) => b - a);
+// sorted_arr;
+// arr;
+// as you see, arr also changed, for preventing this we copy array and then use sort method:
+const sorted_arr2 = arr.slice().sort((a, b) => a - b);
+sorted_arr2;
+arr;
+
+//Add a book to books:
+const newBook = {
+	id: 6,
+	title: 'Harry Potter and the Prisoner of Azkaban',
+	author: 'J. K. Rowlling',
+};
+const bookAfterAdd = [...books, newBook];
+bookAfterAdd;
+
+const bookAfterDelete = bookAfterAdd.filter((book) => book.id !== 3);
+bookAfterDelete;
+//change number of pages for book.id ===1
+const bookAfterUpdate = bookAfterDelete.map(book=> book.id===1? {...book, pages:110}: book);
+bookAfterUpdate;
+*/
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// 	.then((response) => response.json())
+// 	.then((json) => console.log(json));
+
+// console.log('atefe');
+
+async function getData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	const data = await res.json();
+	console.log(data);
+}
+getData()
+console.log('atefe');
